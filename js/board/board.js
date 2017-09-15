8d63dbe7d14da0ac3713addddd7df3c8d252201f
Daimao <damiannkoziel@gmail.com>
2017-09-15 10:57:59 +0200

    Dodana grafika mapy i awatary

--- js/board/board.js
+++ js/board/board.js
@@ -1,29 +1,63 @@
 var Board = {};
 
-var map;
-var grids = []; //TODO pola zwykłe
-var specialGrids = []; //TODO pola specjalne
-var playerGrid=[]; //dane gracza (np. położenie, awatar)
+let map;
+let grids = [[]]; //TODO pola zwykłe
+let specialGrids = []; //TODO pola specjalne
+let players = [];
+let iHeight = window.innerHeight;
+let iWidth = window.innerWidth;
+let mapBackground;
 
 
 Board.preload = function () {
     board.load.image('plansza', 'assets/map/plansza.png'); //załaduj planszę
-    board.load.spritesheet('avatar1','assets/sprites/boy.png');
+    board.load.image('background', 'assets/map/wood_background.png');
+    board.load.spritesheet('avatar1', 'assets/sprites/avatar1.png'); //załaduj awatary
+    board.load.spritesheet('avatar2', 'assets/sprites/avatar2.png');
+    board.load.spritesheet('avatar3', 'assets/sprites/avatar3.png');
+    board.load.spritesheet('avatar4', 'assets/sprites/avatar4.png');
+    board.load.spritesheet('avatar5', 'assets/sprites/avatar5.png');
+    board.load.spritesheet('avatar6', 'assets/sprites/avatar6.png');
 };
 
 Board.create = function () {
-    board.stage.backgroundColor = "#4488AA";
-    map = board.add.image(board.world.centerX,board.world.centerY,'plansza');
-    map.anchor.setTo(0.5, 0.45);
+    mapBackground = board.add.tileSprite(0, 0, 1600, 900, "background");
+    map = board.add.image(iWidth, iHeight, 'plansza');
+    map.anchor.setTo(0.38, 1.0085); //położenie lewej górnej krawędzi obrazka - ta wartość będzie ulegać zmianie
+    map.scale.setTo(0.9);
 
-    playerGrid[1] = board.add.sprite(32,32,'avatar1');
-    playerGrid[1].anchor.setTo(-6.5, -9); //ustawienie gracza na start
+    addPlayersToBoard(6);
+
+    //board.camera.follow(players[0]);
 };
 
 Board.update = function () {
-
+//players[0].body.velocity.x = 250;
 };
 
 Board.render = function () {
 
 };
+
+function addPlayersToBoard(numberOfPlayers) {
+
+    switch (numberOfPlayers) {
+        case 6:
+            players[5] = board.add.sprite(205.1282, 533, 'avatar6');
+        case 5:
+            players[4] = board.add.sprite(168.4210, 533, 'avatar5');
+        case 4:
+            players[3] = board.add.sprite(130.0813, 533, 'avatar4');
+        case 3:
+            players[2] = board.add.sprite(205.1282, 485, 'avatar3');
+        case 2:
+            players[1] = board.add.sprite(168.4210, 485, 'avatar2');
+        case 1:
+            players[0] = board.add.sprite(130.0813, 485, 'avatar1');
+
+    }
+    }
+
+function changePlayerPosition(players, x, y) {
+    players.anchor.setTo(x,y);
+}
--- js/board/mainBoard.js
+++ js/board/mainBoard.js
@@ -1,4 +1,4 @@
-var board = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, document.getElementById('board'));
+var board = new Phaser.Game(window.innerWidth-15.5, window.innerHeight-15.5, Phaser.AUTO, document.getElementById('board'));
 
 board.state.add('Board', Board);
 board.state.start('Board');
