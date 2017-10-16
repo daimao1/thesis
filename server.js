20d70a8378835f5d7e0a8498bd0e2f0680ce1023
Daimao <damiannkoziel@gmail.com>
2017-10-16 17:19:50 +0200

    Enable wwwhisper authorization6

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser')
 var passport = require('passport')
 var flash = require('connect-flash')
 var io = require('socket.io').listen(server)
+
 //resources TODO nie wiadomo czy nam to potrzebne
 app.use('/css', express.static(__dirname + '/css'))
 app.use('/js', express.static(__dirname + '/js'))
