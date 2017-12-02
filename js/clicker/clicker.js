0b1ee123a967874ebd0d040c01f8b870fa21f25b
Tobiasz K <tkaciuczyk@gmail.com>
2017-12-02 21:35:03 +0100

    Update clicker.js

--- js/clicker/clicker.js
+++ js/clicker/clicker.js
@@ -9,3 +9,17 @@ let numberOfPlayers = 6;
 document.addEventListener('DOMContentLoaded', function() {
     numberOfPlayers = +document.getElementById("numberOfPlayers").innerHTML;
 });
+
+
+let timeLeft = 20;
+let downloadTimer = setInterval(function () {
+    document.getElementById("progressBar").value = 20 - --timeLeft;
+    document.getElementById("timeValue").textContent = timeLeft + "";
+    if (timeLeft === 5) {
+        document.getElementById("timeValue").style.color = "red";
+    }
+    if (timeLeft <= 0) {
+        clearInterval(downloadTimer);
+        setTimeout(checkIsServerTimerEnd, 5000);
+    }
+}, 1000);
