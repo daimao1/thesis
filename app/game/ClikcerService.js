b2fdf892907e055afb1a61a0f77f420d77f1ebeb
Tobiasz K <tkaciuczyk@gmail.com>
2017-12-02 18:51:17 +0100

    Add most important functions to ClickerService

--- app/game/ClikcerService.js
+++ app/game/ClikcerService.js
@@ -1,6 +1,59 @@
 'use strict';
+const RoomService = require('../room/RoomService');
 
-exports.initClicker = function () {
+exports.initClicker = initClicker;
 
 
-};
+function initClicker(socketNamespace) {
+    const players = RoomService.getAllPlayersFromRoom(socketNamespace.roomId);
+    let results = new Array(players.length);
+    let isAllPlayersAnswered = false;
+    players.forEach((player) => {
+        player.socket.emit('launchClicker');
+
+        player.socket.on('clickerResult', (result) => {
+            results[player.in_room_id] = result;
+            isAllPlayersAnswered = checkIsAllPlayersSentResults(socketNamespace, results);
+        });
+    });
+}
+
+function checkIsAllPlayersSentResults(socketNamespace, results){
+    if(results.contains(undefined)){
+        return false;
+    } else {
+        collectResults(socketNamespace, results)
+        return true;
+    }
+}
+
+function startClickerTimer(socketNamespace) {
+    const players = RoomService.getAllPlayersFromRoom(socketNamespace.roomId);
+    players.forEach((player) => {
+        player.socket.emit('startClickerTimer');
+    });
+}
+
+function stopClickerTimer(socketNamespace) {
+    const players = RoomService.getAllPlayersFromRoom(socketNamespace.roomId);
+    players.forEach((player) => {
+        player.socket.emit('stopClickerTimer');
+    });
+}
+
+function fillEmptyResults(results){
+    for(let i=0; i< results.length; i++){
+        if(results[i] === undefined){
+            results = 0;
+        }
+    }
+    return results;
+}
+
+
+function collectResults(socketNamespace, results) {
+
+    const playersDTOs = RoomService.getPlayersDTOs(socketNamespace.roomId);
+
+    if(results.length === playersDTOs.length)
+}
