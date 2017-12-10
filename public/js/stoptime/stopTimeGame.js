8ad71b5bc506a8397b72f8e517d7ea2b03478b96
Daimao <damiannkoziel@gmail.com>
2017-12-10 16:21:12 +0100

    fixed bugs

--- public/js/stoptime/stopTimeGame.js
+++ public/js/stoptime/stopTimeGame.js
@@ -1,7 +1,8 @@
 let StopTimeGame = {}
 let roomId = document.head.id
 
-let player1, player2, player3, player4, player5, player6
+// let player1, player2, player3, player4, player5, player6
+let players = []
 let text
 let iHeight
 let iWidth
@@ -22,6 +23,7 @@ let startMessage
 let numberOfPlayersStopped
 let timerSound
 let socketResultsEmitted = false
+let extraTimeForCounter
 
 WebFontConfig = {
 
@@ -35,7 +37,7 @@ WebFontConfig = {
     families: ['Raleway']
   }
 
-};
+}
 
 StopTimeGame.preload = function () {
   stopTimeGame.load.spritesheet('button_blue', '../../assets/buttons/circle_blue.png')
@@ -58,7 +60,7 @@ StopTimeGame.preload = function () {
 
   stopTimeGame.load.audio('timerSound', '../../assets/audio/timerSound.mp3')
 
-  stopTimeGame.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+  stopTimeGame.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js')
 
   socket = io.connect('/' + roomId)
   socket.emit('markStopTimeGame')
@@ -66,7 +68,7 @@ StopTimeGame.preload = function () {
 }
 
 StopTimeGame.create = function () {
-  stopTimeGame.stage.disableVisibilityChange = true; //gra działa gdy okno przeglądarki jest nieaktywne
+  stopTimeGame.stage.disableVisibilityChange = true
   iHeight = window.innerHeight
   iWidth = window.innerWidth
   timerSound = stopTimeGame.add.audio('timerSound')
@@ -75,8 +77,10 @@ StopTimeGame.create = function () {
   numberOfPlayersStopped = 0
   stopTimeGame.add.image('bg').anchor.set(0.5)
 
+
   goFullScreen()
   random_number = randomInt(7, 17)
+  extraTimeForCounter = Math.ceil(random_number / 2)
   createButtonsWithAvatars(numberOfPlayers)
   resetPlayersTime(numberOfPlayers)
   showInitMessage()
@@ -84,19 +88,18 @@ StopTimeGame.create = function () {
 }
 
 StopTimeGame.render = function () {
+  if (numberOfPlayers !== undefined && random_number !== undefined) {
 
-  if(numberOfPlayers !== undefined && random_number !== undefined) {
-      for (let i = 0; i < numberOfPlayers; i++) {
-          if(textCounter[i] !== undefined) {
-              counter[i] = Math.abs(random_number*1000 + 7000 - timerEvents[i].duration.toFixed(0));
-              textCounter[i].setText((counter[i]))
-              generalCounter = generalTimer.duration
-          }
+    for (let i = 0; i < numberOfPlayers; i++) {
+      if (textCounter[i] !== undefined && generalTimer.running && !players[i].isStopped) {
+        counter[i] = ((random_number * 1000 + extraTimeForCounter * 1000 - generalTimer.duration) / 1000).toFixed(2)
+        textCounter[i].setText((counter[i]))
       }
+    }
   }
 }
 
-function startGame(){
+function startGame () {
   setTimeout(function () {
     showTimeDestinationText(random_number)
   }, 2700)
@@ -114,28 +117,29 @@ function startGame(){
     startMessage.destroy()
   }, 6800)
 
-  let temp = 6800 + (random_number * 1000) + 7000
+}
 
-  setTimeout(function () {
-    if(socketResultsEmitted === false) {
-      let random_number_ms = random_number * 1000
-      for (let i = 0; i < numberOfPlayers; i++)
-        timerEvents[i].pause()
-      // console.log('COUNTER: ' + generalCounter)
-      showResultsOnScreen()
-      let arrayPlayers = arrayWithResults()
-      console.log('random_number_ms ' + random_number_ms + 'arrayPlayers ' + arrayPlayers)
-      socket.emit('stopTimeResults', random_number_ms, arrayPlayers)
-      redirectToBoard()
-      // console.log('Wyslano emit stopTimeResults')
+function onTimerEnd () {
+
+  generalTimer.stop()
+  for (let i = 0; i < numberOfPlayers; i++) {
+    if (!players[i].isStopped) {
+      textCounter[i].setText((random_number + extraTimeForCounter).toFixed(2))
     }
-  }, temp)
+  }
+
+  if (socketResultsEmitted === false) {
+    let random_number_ms = random_number * 100
+    showResultsOnScreen()
+    let arrayPlayers = arrayWithResults()
+    console.log('random_number_ms ' + random_number_ms + 'arrayPlayers ' + arrayPlayers)
+    socket.emit('stopTimeResults', random_number_ms, arrayPlayers)
+  }
 }
 
 function makeTimers (number) {
   generalTimer = stopTimeGame.time.create(false)
-  generalTimer.loop(random_number * 1000 + 7000, updateGeneralCounter, this)
-  generalTimer.start()
+  generalTimer.loop(random_number * 1000 + extraTimeForCounter * 1000, onTimerEnd, this)
 
   for (let i = 0; i < number; i++) {
     counter[i] = 0
@@ -173,44 +177,15 @@ function makeTimers (number) {
         break
 
     }
-    timerEvents[i] = stopTimeGame.time.create(false)
-    //timerEvents[i].loop(Phaser.Timer.SECOND / 100, updateCounter, this, i)
-      timerEvents[i].loop(random_number * 1000 + 7000, updateCounter, this, i)
-    timerEvents[i].start()
+    generalTimer.start()
   }
 }
 
-function updateGeneralCounter () {
-
-}
-
-function updateCounter (index) {
-  console.log('timer end'); //TODO remove
-  //counter[index]++
-  //textCounter[index].setText((counter[index] * 0.01).toFixed(2))
-  //console.log(counter[index])
-}
-
-function resetPlayersTime (number) {
-  let tempTime = (random_number + 7) * 100
-  switch (number) {
-    case 6:
-      player6.time = tempTime
-      player6.isStopped = false
-    case 5:
-      player5.time = tempTime
-      player5.isStopped = false
-    case 4:
-      player4.time = tempTime
-      player4.isStopped = false
-    case 3:
-      player3.time = tempTime
-      player3.isStopped = false
-    case 2:
-      player2.time = tempTime
-      player2.isStopped = false
-      player1.time = tempTime
-      player1.isStopped = false
+function resetPlayersTime () {
+  let tempTime = (random_number + extraTimeForCounter) * 1000
+  for (let i = 0; i < numberOfPlayers; i++) {
+    players[i].time = tempTime
+    players[i].isStopped = false
   }
 }
 
@@ -218,9 +193,9 @@ StopTimeGame.update = function () {
 }
 
 function setHandlers () {
-  socket.on('playersInfo', playersInfo)
-  socket.on('stopPlayerButton', stopButton)
-  socket.on('clientReady',startGame)
+  socket.once('playersInfo', playersInfo)
+  socket.once('stopPlayerButton', stopButton)
+  socket.once('clientReady', startGame)
 }
 
 function playersInfo (players) {
@@ -289,35 +264,35 @@ function randomInt (low, high) {
 function addAvatars (number) {
   switch (number) {
     case 2:
-      player1 = stopTimeGame.add.sprite(iWidth * 0.37, iHeight * 0.57, 'avatar1')
-      player2 = stopTimeGame.add.sprite(iWidth * 0.56, iHeight * 0.57, 'avatar2')
+      players[0] = stopTimeGame.add.sprite(iWidth * 0.37, iHeight * 0.57, 'avatar1')
+      players[1] = stopTimeGame.add.sprite(iWidth * 0.56, iHeight * 0.57, 'avatar2')
       showText(allPlayers[0].name, 0.35, 0.45)
       showText(allPlayers[1].name, 0.54, 0.45)
       break
     case 3:
-      player1 = stopTimeGame.add.sprite(iWidth * 0.28, iHeight * 0.57, 'avatar1')
-      player2 = stopTimeGame.add.sprite(iWidth * 0.46, iHeight * 0.57, 'avatar2')
-      player3 = stopTimeGame.add.sprite(iWidth * 0.63, iHeight * 0.57, 'avatar3')
+      players[0] = stopTimeGame.add.sprite(iWidth * 0.28, iHeight * 0.57, 'avatar1')
+      players[1] = stopTimeGame.add.sprite(iWidth * 0.46, iHeight * 0.57, 'avatar2')
+      players[2] = stopTimeGame.add.sprite(iWidth * 0.63, iHeight * 0.57, 'avatar3')
       showText(allPlayers[0].name, 0.26, 0.45)
       showText(allPlayers[1].name, 0.44, 0.45)
       showText(allPlayers[2].name, 0.61, 0.45)
       break
     case 4:
-      player1 = stopTimeGame.add.sprite(iWidth * 0.19, iHeight * 0.57, 'avatar1')
-      player2 = stopTimeGame.add.sprite(iWidth * 0.37, iHeight * 0.57, 'avatar2')
-      player3 = stopTimeGame.add.sprite(iWidth * 0.54, iHeight * 0.57, 'avatar3')
-      player4 = stopTimeGame.add.sprite(iWidth * 0.72, iHeight * 0.57, 'avatar4')
+      players[0] = stopTimeGame.add.sprite(iWidth * 0.19, iHeight * 0.57, 'avatar1')
+      players[1] = stopTimeGame.add.sprite(iWidth * 0.37, iHeight * 0.57, 'avatar2')
+      players[2] = stopTimeGame.add.sprite(iWidth * 0.54, iHeight * 0.57, 'avatar3')
+      players[3] = stopTimeGame.add.sprite(iWidth * 0.72, iHeight * 0.57, 'avatar4')
       showText(allPlayers[0].name, 0.17, 0.45)
       showText(allPlayers[1].name, 0.35, 0.45)
       showText(allPlayers[2].name, 0.52, 0.45)
       showText(allPlayers[3].name, 0.70, 0.45)
       break
     case 5:
-      player1 = stopTimeGame.add.sprite(iWidth * 0.13, iHeight * 0.57, 'avatar1')
-      player2 = stopTimeGame.add.sprite(iWidth * 0.30, iHeight * 0.57, 'avatar2')
-      player3 = stopTimeGame.add.sprite(iWidth * 0.47, iHeight * 0.57, 'avatar3')
-      player4 = stopTimeGame.add.sprite(iWidth * 0.65, iHeight * 0.57, 'avatar4')
-      player5 = stopTimeGame.add.sprite(iWidth * 0.81, iHeight * 0.57, 'avatar5')
+      players[0] = stopTimeGame.add.sprite(iWidth * 0.13, iHeight * 0.57, 'avatar1')
+      players[1] = stopTimeGame.add.sprite(iWidth * 0.30, iHeight * 0.57, 'avatar2')
+      players[2] = stopTimeGame.add.sprite(iWidth * 0.47, iHeight * 0.57, 'avatar3')
+      players[3] = stopTimeGame.add.sprite(iWidth * 0.65, iHeight * 0.57, 'avatar4')
+      players[4] = stopTimeGame.add.sprite(iWidth * 0.81, iHeight * 0.57, 'avatar5')
       showText(allPlayers[0].name, 0.11, 0.45)
       showText(allPlayers[1].name, 0.28, 0.45)
       showText(allPlayers[2].name, 0.46, 0.45)
@@ -325,12 +300,12 @@ function addAvatars (number) {
       showText(allPlayers[4].name, 0.79, 0.45)
       break
     case 6:
-      player1 = stopTimeGame.add.sprite(iWidth * 0.05, iHeight * 0.45, 'avatar1')
-      player2 = stopTimeGame.add.sprite(iWidth * 0.22, iHeight * 0.45, 'avatar2')
-      player3 = stopTimeGame.add.sprite(iWidth * 0.39, iHeight * 0.45, 'avatar3')
-      player4 = stopTimeGame.add.sprite(iWidth * 0.56, iHeight * 0.45, 'avatar4')
-      player5 = stopTimeGame.add.sprite(iWidth * 0.73, iHeight * 0.45, 'avatar5')
-      player6 = stopTimeGame.add.sprite(iWidth * 0.90, iHeight * 0.45, 'avatar6')
+      players[0] = stopTimeGame.add.sprite(iWidth * 0.05, iHeight * 0.45, 'avatar1')
+      players[1] = stopTimeGame.add.sprite(iWidth * 0.22, iHeight * 0.45, 'avatar2')
+      players[2] = stopTimeGame.add.sprite(iWidth * 0.39, iHeight * 0.45, 'avatar3')
+      players[3] = stopTimeGame.add.sprite(iWidth * 0.56, iHeight * 0.45, 'avatar4')
+      players[4] = stopTimeGame.add.sprite(iWidth * 0.73, iHeight * 0.45, 'avatar5')
+      players[5] = stopTimeGame.add.sprite(iWidth * 0.90, iHeight * 0.45, 'avatar6')
       showText(allPlayers[0].name, 0.03, 0.4)
       showText(allPlayers[1].name, 0.21, 0.4)
       showText(allPlayers[2].name, 0.38, 0.4)
@@ -381,138 +356,67 @@ function showStartMessage () {
 }
 
 function stopButton (playerId) {
-  timerEvents[playerId].pause()
+  players[playerId].time = counter[playerId]
+  players[playerId].isStopped = true
+
   switch (playerId) {
     case 5:
-      player6.time = counter[playerId]
-      player6.isStopped = true
       button_pink.alpha = 0.4
       break
     case 4:
-      player5.time = counter[playerId]
-      player5.isStopped = true
       button_orange.alpha = 0.4
       break
     case 3:
-      player4.time = counter[playerId]
-      player4.isStopped = true
       button_green.alpha = 0.4
       break
     case 2:
-      player3.time = counter[playerId]
-      player3.isStopped = true
       button_red.alpha = 0.4
       break
     case 1:
-      player2.time = counter[playerId]
-      player2.isStopped = true
       button_yellow.alpha = 0.4
       break
     case 0:
-      player1.time = counter[playerId]
-      player1.isStopped = true
       button_blue.alpha = 0.4
       break
   }
-  timerEvents[playerId].destroy(); //TODO nie wiem czy to będzie dobre
+
   numberOfPlayersStopped++
   if (numberOfPlayersStopped === numberOfPlayers) {
     showResultsOnScreen()
     let arrayPlayers = arrayWithResults()
-    let random_number_ms = random_number * 1000
+    let random_number_ms = random_number * 100
     socket.emit('stopTimeResults', random_number_ms, arrayPlayers)
-    socketResultsEmitted = true;
+    socketResultsEmitted = true
     console.log('Wyslano emit stopTimeResults ' + 'random_number_ms: ' + random_number_ms + ' arrayPlayers' + arrayPlayers)
     console.log('Pierwszy gracz czas: ' + counter[0])
     console.log()
+    //redirectToBoard();
   }
 }
 
 function showResultsOnScreen () {
-  switch (numberOfPlayers) {
-    case 2:
-      console.log('player1.time: ' +player1.time.toFixed(2) / 100)
-      showTextCounter(0, 0.38, 0.75, player1.time.toFixed(2) / 100)
-      showTextCounter(1, 0.57, 0.75, player2.time.toFixed(2) / 100)
-      break
-    case 3:
-      showTextCounter(0, 0.29, 0.75, player1.time)
-      showTextCounter(1, 0.47, 0.75, player2.time)
-      showTextCounter(2, 0.64, 0.75, player3.time)
-      break
-    case 4:
-      showTextCounter(0, 0.2, 0.75, player1.time.toFixed(2))
-      showTextCounter(1, 0.38, 0.75, player2.time.toFixed(2))
-      showTextCounter(2, 0.55, 0.75, player3.time.toFixed(2))
-      showTextCounter(3, 0.73, 0.75, player4.time.toFixed(2))
-      break
-    case 5:
-      showTextCounter(0, 0.14, 0.75, player1.time.toFixed(2))
-      showTextCounter(1, 0.31, 0.75, player2.time.toFixed(2))
-      showTextCounter(2, 0.48, 0.75, player3.time.toFixed(2))
-      showTextCounter(3, 0.66, 0.75, player4.time.toFixed(2))
-      showTextCounter(4, 0.82, 0.75, player5.time.toFixed(2))
-      break
-    case 6:
-      showTextCounter(0, 0.05, 0.75, player1.time.toFixed(2))
-      showTextCounter(1, 0.23, 0.75, player2.time.toFixed(2))
-      showTextCounter(2, 0.40, 0.75, player3.time.toFixed(2))
-      showTextCounter(3, 0.57, 0.75, player4.time.toFixed(2))
-      showTextCounter(4, 0.74, 0.75, player5.time.toFixed(2))
-      showTextCounter(5, 0.91, 0.75, player6.time.toFixed(2))
-      break
+  for (let i = 0; i < numberOfPlayers; i++) {
+    textCounter[i].visible = true
   }
-  redirectToBoard();
+
 }
 
 function arrayWithResults () {
   let arrayWithPlayers = []
-  let temp = (random_number + 7) * 1000
+  let temp = (random_number + extraTimeForCounter) * 100
+
   for (let i = 0; i < numberOfPlayers; i++) {
-    arrayWithPlayers[i] = counter[i] * 10
+    arrayWithPlayers[i] = counter[i] * 100
+    if (!players[i].isStopped) {
+      players[i].time = temp
+      arrayWithPlayers[i] = temp
+    }
   }
-
-  switch(numberOfPlayers){
-    case 6:
-      if(player6.isStopped === false){
-        player6.time = temp
-        arrayWithPlayers[5] = temp;
-      }
-    case 5:
-      if(player5.isStopped === false){
-        player5.time = temp
-        arrayWithPlayers[4] = temp;
-      }
-    case 4:
-
-      if(player4.isStopped === false){
-        player4.time = temp
-        arrayWithPlayers[3] = temp;
-      }
-    case 3:
-      if(player3.isStopped === false){
-        player3.time = temp
-        arrayWithPlayers[2] = temp;
-      }
-    case 2:
-      if(player1.isStopped === false){
-        player1.time = temp
-        arrayWithPlayers[1] = temp;
-      }
-      console.log('temp '+temp)
-      if(player2.isStopped === false){
-        player2.time = temp
-        arrayWithPlayers[0] = temp;
-      }
-      break
-  }
-
   return arrayWithPlayers
-
 }
 
-function redirectToBoard() {
+function redirectToBoard () {
   setTimeout(function () {
-    window.location.href = "/board/" + roomId;
-  }, 7000);
+    window.location.href = '/board/' + roomId
+  }, 7000)
 }
