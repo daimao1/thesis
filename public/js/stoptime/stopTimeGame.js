5e01dc9f5eaba8bb9a8afe5a353f9f324a96731d
Tobiasz K <tkaciuczyk@gmail.com>
2017-12-11 01:20:38 +0100

    Update stopTimeGame - new layout of positioning all elements

--- public/js/stoptime/stopTimeGame.js
+++ public/js/stoptime/stopTimeGame.js
@@ -56,7 +56,6 @@ StopTimeGame.create = function () {
     timerSound.volume = 0.3
     numberOfPlayersStopped = 0
     stopTimeGame.add.sprite(0, 0, 'bg')
-    //showTextCountersWithZeros();
 
     goFullScreen()
     random_number = randomInt(7, 17)
@@ -138,41 +137,40 @@ function startTimers(number) {
     for (let i = 0; i < number; i++) {
         counter[i] = 0
     }
-    //showAllTextCounters(number);
     generalTimer.start()
 }
 
 function showAllTextCounters(number) {
     switch (number) {
         case 6:
-            showTextCounter(0, 0.05, 0.76, '')
-            showTextCounter(1, 0.23, 0.76, '')
-            showTextCounter(2, 0.40, 0.76, '')
-            showTextCounter(3, 0.57, 0.76, '')
-            showTextCounter(4, 0.74, 0.76, '')
-            showTextCounter(5, 0.91, 0.76, '')
+            showTextCounter(0, 0.09, 0.77, '')
+            showTextCounter(1, 0.25, 0.77, '')
+            showTextCounter(2, 0.42, 0.77, '')
+            showTextCounter(3, 0.58, 0.77, '')
+            showTextCounter(4, 0.75, 0.77, '')
+            showTextCounter(5, 0.91, 0.77, '')
             break
         case 5:
-            showTextCounter(0, 0.14, 0.76, '')
-            showTextCounter(1, 0.31, 0.76, '')
-            showTextCounter(2, 0.48, 0.76, '')
-            showTextCounter(3, 0.66, 0.76, '')
-            showTextCounter(4, 0.82, 0.76, '')
+            showTextCounter(0, 0.1, 0.77, '')
+            showTextCounter(1, 0.3, 0.77, '')
+            showTextCounter(2, 0.5, 0.77, '')
+            showTextCounter(3, 0.7, 0.77, '')
+            showTextCounter(4, 0.9, 0.77, '')
             break
         case 4:
-            showTextCounter(0, 0.2, 0.76, '')
-            showTextCounter(1, 0.38, 0.76, '')
-            showTextCounter(2, 0.55, 0.76, '')
-            showTextCounter(3, 0.73, 0.76, '')
+            showTextCounter(0, 0.14, 0.77, '')
+            showTextCounter(1, 0.38, 0.77, '')
+            showTextCounter(2, 0.62, 0.77, '')
+            showTextCounter(3, 0.86, 0.77, '')
             break
         case 3:
-            showTextCounter(0, 0.29, 0.76, '')
-            showTextCounter(1, 0.47, 0.76, '')
-            showTextCounter(2, 0.64, 0.75, '')
+            showTextCounter(0, 0.2, 0.77, '')
+            showTextCounter(1, 0.5, 0.77, '')
+            showTextCounter(2, 0.8, 0.77, '')
             break
         case 2:
-            showTextCounter(0, 0.33, 0.76, '')
-            showTextCounter(1, 0.52, 0.76, '')
+            showTextCounter(0, 0.35, 0.77, '')
+            showTextCounter(1, 0.65, 0.77, '')
             break
     }
 }
@@ -227,34 +225,54 @@ function onLastTweenComplete() {
 function createButtonsWithAvatars(numberOfPlayers) {
     switch (numberOfPlayers) {
         case 6:
-            button_blue = stopTimeGame.add.button(iWidth * 0.03, iHeight * 0.57, 'button_blue')
-            button_yellow = stopTimeGame.add.button(iWidth * 0.20, iHeight * 0.57, 'button_yellow')
-            button_red = stopTimeGame.add.button(iWidth * 0.37, iHeight * 0.57, 'button_red')
-            button_green = stopTimeGame.add.button(iWidth * 0.54, iHeight * 0.57, 'button_green')
-            button_orange = stopTimeGame.add.button(iWidth * 0.71, iHeight * 0.57, 'button_orange')
-            button_pink = stopTimeGame.add.button(iWidth * 0.88, iHeight * 0.57, 'button_pink')
+            button_blue = stopTimeGame.add.button(iWidth * 0.09, iHeight * 0.57, 'button_blue')
+            button_blue.anchor.x = 0.5;
+            button_yellow = stopTimeGame.add.button(iWidth * 0.25, iHeight * 0.57, 'button_yellow')
+            button_yellow.anchor.x = 0.5;
+            button_red = stopTimeGame.add.button(iWidth * 0.42, iHeight * 0.57, 'button_red')
+            button_red.anchor.x = 0.5;
+            button_green = stopTimeGame.add.button(iWidth * 0.58, iHeight * 0.57, 'button_green')
+            button_green.anchor.x = 0.5;
+            button_orange = stopTimeGame.add.button(iWidth * 0.75, iHeight * 0.57, 'button_orange')
+            button_orange.anchor.x = 0.5;
+            button_pink = stopTimeGame.add.button(iWidth * 0.91, iHeight * 0.57, 'button_pink')
+            button_pink.anchor.x = 0.5;
             break
         case 5:
-            button_blue = stopTimeGame.add.button(iWidth * 0.11, iHeight * 0.57, 'button_blue')
-            button_yellow = stopTimeGame.add.button(iWidth * 0.28, iHeight * 0.57, 'button_yellow')
-            button_red = stopTimeGame.add.button(iWidth * 0.45, iHeight * 0.57, 'button_red')
-            button_green = stopTimeGame.add.button(iWidth * 0.63, iHeight * 0.57, 'button_green')
-            button_orange = stopTimeGame.add.button(iWidth * 0.79, iHeight * 0.57, 'button_orange')
+            button_blue = stopTimeGame.add.button(iWidth * 0.1, iHeight * 0.57, 'button_blue')
+            button_blue.anchor.x = 0.5;
+            button_yellow = stopTimeGame.add.button(iWidth * 0.3, iHeight * 0.57, 'button_yellow')
+            button_yellow.anchor.x = 0.5;
+            button_red = stopTimeGame.add.button(iWidth * 0.5, iHeight * 0.57, 'button_red')
+            button_red.anchor.x = 0.5;
+            button_green = stopTimeGame.add.button(iWidth * 0.7, iHeight * 0.57, 'button_green')
+            button_green.anchor.x = 0.5;
+            button_orange = stopTimeGame.add.button(iWidth * 0.9, iHeight * 0.57, 'button_orange')
+            button_orange.anchor.x = 0.5;
             break
         case 4:
-            button_blue = stopTimeGame.add.button(iWidth * 0.17, iHeight * 0.57, 'button_blue')
-            button_yellow = stopTimeGame.add.button(iWidth * 0.35, iHeight * 0.57, 'button_yellow')
-            button_red = stopTimeGame.add.button(iWidth * 0.52, iHeight * 0.57, 'button_red')
-            button_green = stopTimeGame.add.button(iWidth * 0.70, iHeight * 0.57, 'button_green')
+            button_blue = stopTimeGame.add.button(iWidth * 0.14, iHeight * 0.57, 'button_blue')
+            button_blue.anchor.x = 0.5;
+            button_yellow = stopTimeGame.add.button(iWidth * 0.38, iHeight * 0.57, 'button_yellow')
+            button_yellow.anchor.x = 0.5;
+            button_red = stopTimeGame.add.button(iWidth * 0.62, iHeight * 0.57, 'button_red')
+            button_red.anchor.x = 0.5;
+            button_green = stopTimeGame.add.button(iWidth * 0.86, iHeight * 0.57, 'button_green')
+            button_green.anchor.x = 0.5;
             break
         case 3:
-            button_blue = stopTimeGame.add.button(iWidth * 0.26, iHeight * 0.57, 'button_blue')
-            button_yellow = stopTimeGame.add.button(iWidth * 0.44, iHeight * 0.57, 'button_yellow')
-            button_red = stopTimeGame.add.button(iWidth * 0.61, iHeight * 0.57, 'button_red')
+            button_blue = stopTimeGame.add.button(iWidth * 0.2, iHeight * 0.57, 'button_blue')
+            button_blue.anchor.x = 0.5;
+            button_yellow = stopTimeGame.add.button(iWidth * 0.5, iHeight * 0.57, 'button_yellow')
+            button_yellow.anchor.x = 0.5;
+            button_red = stopTimeGame.add.button(iWidth * 0.8, iHeight * 0.57, 'button_red')
+            button_red.anchor.x = 0.5;
             break
         case 2:
             button_blue = stopTimeGame.add.button(iWidth * 0.35, iHeight * 0.57, 'button_blue')
-            button_yellow = stopTimeGame.add.button(iWidth * 0.54, iHeight * 0.57, 'button_yellow')
+            button_blue.anchor.x = 0.5;
+            button_yellow = stopTimeGame.add.button(iWidth * 0.65, iHeight * 0.57, 'button_yellow')
+            button_yellow.anchor.x = 0.5;
             break
     }
     addAvatars(numberOfPlayers)
@@ -267,97 +285,125 @@ function randomInt(low, high) {
 function addAvatars(number) {
     switch (number) {
         case 2:
-            players[0] = stopTimeGame.add.sprite(iWidth * 0.37, iHeight * 0.57, 'avatar1')
-            players[1] = stopTimeGame.add.sprite(iWidth * 0.56, iHeight * 0.57, 'avatar2')
-            showText(allPlayers[0].name, 0.35, 0.45)
-            showText(allPlayers[1].name, 0.54, 0.45)
+            players[0] = stopTimeGame.add.sprite(iWidth * 0.35, iHeight * 0.57, 'avatar1')
+            players[0].anchor.x = 0.5
+            players[1] = stopTimeGame.add.sprite(iWidth * 0.65, iHeight * 0.57, 'avatar2')
+            players[1].anchor.x = 0.5
+            showText(allPlayers[0].name, 0.35, 0.46)
+            showText(allPlayers[1].name, 0.65, 0.46)
             break
         case 3:
-            players[0] = stopTimeGame.add.sprite(iWidth * 0.28, iHeight * 0.57, 'avatar1')
-            players[1] = stopTimeGame.add.sprite(iWidth * 0.46, iHeight * 0.57, 'avatar2')
-            players[2] = stopTimeGame.add.sprite(iWidth * 0.63, iHeight * 0.57, 'avatar3')
-            showText(allPlayers[0].name, 0.26, 0.45)
-            showText(allPlayers[1].name, 0.44, 0.45)
-            showText(allPlayers[2].name, 0.61, 0.45)
+            players[0] = stopTimeGame.add.sprite(iWidth * 0.2, iHeight * 0.57, 'avatar1')
+            players[0].anchor.x = 0.5
+            players[1] = stopTimeGame.add.sprite(iWidth * 0.5, iHeight * 0.57, 'avatar2')
+            players[1].anchor.x = 0.5
+            players[2] = stopTimeGame.add.sprite(iWidth * 0.8, iHeight * 0.57, 'avatar3')
+            players[2].anchor.x = 0.5
+            showText(allPlayers[0].name, 0.2, 0.46)
+            showText(allPlayers[1].name, 0.5, 0.46)
+            showText(allPlayers[2].name, 0.8, 0.46)
             break
         case 4:
-            players[0] = stopTimeGame.add.sprite(iWidth * 0.19, iHeight * 0.57, 'avatar1')
-            players[1] = stopTimeGame.add.sprite(iWidth * 0.37, iHeight * 0.57, 'avatar2')
-            players[2] = stopTimeGame.add.sprite(iWidth * 0.54, iHeight * 0.57, 'avatar3')
-            players[3] = stopTimeGame.add.sprite(iWidth * 0.72, iHeight * 0.57, 'avatar4')
-            showText(allPlayers[0].name, 0.17, 0.45)
-            showText(allPlayers[1].name, 0.35, 0.45)
-            showText(allPlayers[2].name, 0.52, 0.45)
-            showText(allPlayers[3].name, 0.70, 0.45)
+            players[0] = stopTimeGame.add.sprite(iWidth * 0.14, iHeight * 0.57, 'avatar1')
+            players[0].anchor.x = 0.5
+            players[1] = stopTimeGame.add.sprite(iWidth * 0.38, iHeight * 0.57, 'avatar2')
+            players[1].anchor.x = 0.5
+            players[2] = stopTimeGame.add.sprite(iWidth * 0.62, iHeight * 0.57, 'avatar3')
+            players[2].anchor.x = 0.5
+            players[3] = stopTimeGame.add.sprite(iWidth * 0.86, iHeight * 0.57, 'avatar4')
+            players[3].anchor.x = 0.5
+            showText(allPlayers[0].name, 0.14, 0.46)
+            showText(allPlayers[1].name, 0.38, 0.46)
+            showText(allPlayers[2].name, 0.62, 0.46)
+            showText(allPlayers[3].name, 0.86, 0.46)
             break
         case 5:
-            players[0] = stopTimeGame.add.sprite(iWidth * 0.13, iHeight * 0.57, 'avatar1')
-            players[1] = stopTimeGame.add.sprite(iWidth * 0.30, iHeight * 0.57, 'avatar2')
-            players[2] = stopTimeGame.add.sprite(iWidth * 0.47, iHeight * 0.57, 'avatar3')
-            players[3] = stopTimeGame.add.sprite(iWidth * 0.65, iHeight * 0.57, 'avatar4')
-            players[4] = stopTimeGame.add.sprite(iWidth * 0.81, iHeight * 0.57, 'avatar5')
-            showText(allPlayers[0].name, 0.11, 0.45)
-            showText(allPlayers[1].name, 0.28, 0.45)
-            showText(allPlayers[2].name, 0.46, 0.45)
-            showText(allPlayers[3].name, 0.63, 0.45)
-            showText(allPlayers[4].name, 0.79, 0.45)
+            players[0] = stopTimeGame.add.sprite(iWidth * 0.1, iHeight * 0.57, 'avatar1')
+            players[0].anchor.x = 0.5
+            players[1] = stopTimeGame.add.sprite(iWidth * 0.3, iHeight * 0.57, 'avatar2')
+            players[1].anchor.x = 0.5
+            players[2] = stopTimeGame.add.sprite(iWidth * 0.5, iHeight * 0.57, 'avatar3')
+            players[2].anchor.x = 0.5
+            players[3] = stopTimeGame.add.sprite(iWidth * 0.7, iHeight * 0.57, 'avatar4')
+            players[3].anchor.x = 0.5
+            players[4] = stopTimeGame.add.sprite(iWidth * 0.9, iHeight * 0.57, 'avatar5')
+            players[4].anchor.x = 0.5
+            showText(allPlayers[0].name, 0.1, 0.46)
+            showText(allPlayers[1].name, 0.3, 0.46)
+            showText(allPlayers[2].name, 0.5, 0.46)
+            showText(allPlayers[3].name, 0.7, 0.46)
+            showText(allPlayers[4].name, 0.9, 0.46)
             break
         case 6:
-            players[0] = stopTimeGame.add.sprite(iWidth * 0.05, iHeight * 0.45, 'avatar1')
-            players[1] = stopTimeGame.add.sprite(iWidth * 0.22, iHeight * 0.45, 'avatar2')
-            players[2] = stopTimeGame.add.sprite(iWidth * 0.39, iHeight * 0.45, 'avatar3')
-            players[3] = stopTimeGame.add.sprite(iWidth * 0.56, iHeight * 0.45, 'avatar4')
-            players[4] = stopTimeGame.add.sprite(iWidth * 0.73, iHeight * 0.45, 'avatar5')
-            players[5] = stopTimeGame.add.sprite(iWidth * 0.90, iHeight * 0.45, 'avatar6')
-            showText(allPlayers[0].name, 0.03, 0.4)
-            showText(allPlayers[1].name, 0.21, 0.4)
-            showText(allPlayers[2].name, 0.38, 0.4)
-            showText(allPlayers[3].name, 0.54, 0.4)
-            showText(allPlayers[4].name, 0.71, 0.4)
-            showText(allPlayers[5].name, 0.88, 0.4)
+            players[0] = stopTimeGame.add.sprite(iWidth * 0.09, iHeight * 0.57, 'avatar1')
+            players[0].anchor.x = 0.5
+            players[1] = stopTimeGame.add.sprite(iWidth * 0.25, iHeight * 0.57, 'avatar2')
+            players[1].anchor.x = 0.5
+            players[2] = stopTimeGame.add.sprite(iWidth * 0.42, iHeight * 0.57, 'avatar3')
+            players[2].anchor.x = 0.5
+            players[3] = stopTimeGame.add.sprite(iWidth * 0.58, iHeight * 0.57, 'avatar4')
+            players[3].anchor.x = 0.5
+            players[4] = stopTimeGame.add.sprite(iWidth * 0.75, iHeight * 0.57, 'avatar5')
+            players[4].anchor.x = 0.5
+            players[5] = stopTimeGame.add.sprite(iWidth * 0.91, iHeight * 0.57, 'avatar6')
+            players[5].anchor.x = 0.5
+            showText(allPlayers[0].name, 0.09, 0.46)
+            showText(allPlayers[1].name, 0.25, 0.46)
+            showText(allPlayers[2].name, 0.42, 0.46)
+            showText(allPlayers[3].name, 0.58, 0.46)
+            showText(allPlayers[4].name, 0.75, 0.46)
+            showText(allPlayers[5].name, 0.91, 0.46)
     }
 }
 
 function showTextCounter(num, x, y, text) {
     textCounter[num] = stopTimeGame.add.text(iWidth * x, iHeight * y, text, {
         font: 'Orbitron',
-        fontSize: 55,
+        fontSize: 48,
         fill: '#ffffff',
         align: 'center',
         fontWeight: 'bold'
         //backgroundColor: 'rgba(117, 117, 117, 0.7)'
     });
+    textCounter[num].anchor.x = 0.5
 }
 
 function showText(text, x, y) {
-    stopTimeGame.add.text(iWidth * x, iHeight * y, text, {
+    let textToShow = stopTimeGame.add.text(iWidth * x, iHeight * y, text, {
         font: 'Raleway',
         fontSize: 45,
         fill: '#ffffff',
         align: 'center',
         fontWeight: 'bold'
     });
+    textToShow.anchor.x = 0.5
 }
 
 function showInitMessage() {
 
-    initMessage = stopTimeGame.add.bitmapText(1, 1, 'desyrel-pink', 'Przygotuj sie do gry...  ', 64)
+    initMessage = stopTimeGame.add.bitmapText(stopTimeGame.world.centerX, stopTimeGame.world.centerY - 200, 'desyrel-pink', 'Przygotuj sie do gry...', 64)
     initMessage.fontSize = 85
-    initMessage.fixedToCamera = true
-    initMessage.cameraOffset.setTo(iWidth / 4.2, iHeight / 12)
+    initMessage.anchor.set(0.5)
+    //initMessage.fixedToCamera = true
+    //initMessage.cameraOffset.setTo(iWidth / 4.2, iHeight / 12)
 }
 
 function showTimeDestinationText(random_number) {
-    text = stopTimeGame.add.bitmapText(iWidth / 3.6, iHeight / 6.0,
-        'desyrel-pink', 'Traf w: ' + random_number + ' sekund', 85)
+    // text = stopTimeGame.add.bitmapText(iWidth / 3.6, iHeight / 6.0,
+    //     'desyrel-pink', 'Traf w: ' + random_number + ' sekund', 85)
+    text = stopTimeGame.add.bitmapText(stopTimeGame.world.centerX, stopTimeGame.world.centerY - 200,
+        'desyrel-pink', 'Traf w: ' + random_number + ' sekund', 85);
+    text.anchor.set(0.5)
+
     initMessage.destroy()
 }
 
 function showStartMessage() {
-    startMessage = stopTimeGame.add.bitmapText(1, 1, 'desyrel-pink', 'START!  ', 64)
+    startMessage = stopTimeGame.add.bitmapText(stopTimeGame.world.centerX, stopTimeGame.world.centerY - 110, 'desyrel-pink', 'START!  ', 64)
     startMessage.fontSize = 85
-    startMessage.fixedToCamera = true
-    startMessage.cameraOffset.setTo(iWidth / 2.5, iHeight / 3.8)
+    startMessage.anchor.set(0.5)
+    //startMessage.fixedToCamera = true
+    //startMessage.cameraOffset.setTo(iWidth / 2.5, iHeight / 3.8)
 }
 
 function stopButton(playerId) {
